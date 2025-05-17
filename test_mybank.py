7f69111dac6cd8a0e586e2ae1737461443bf62e0
Karuna Issa <karunaissa400@gmail.com>
2025-05-17 20:28:35 +0530

    Create test_mybank.py

---
+++ test_mybank.py
@@ -0,0 +1,25 @@
+import pytest
+from mybank import BankAccount
+
+def test_deposit():
+
+    account = BankAccount(balance=150)
+
+    account.deposit(50)
+
+    assert account.balance == 200
+
+def test_withdrawal_sufficent_amount():
+
+    account = BankAccount(balance=150)
+
+    account.withdrawal(30)
+
+    assert account.balance == 120
+
+def test_withdrawal_insufficent_amount():
+
+    account = BankAccount(balance=150)
+
+    with pytest.raises(ValueError, match="Insufficent Funds")
+    account.withdrawal(200)
